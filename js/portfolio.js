1a198067df9cc073dedbe8b45f641dce0f94353d
maarekua <markiyan89@gmail.com>
2019-12-16 23:11:23 +0000

    dimkino

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,35 @@
 const buttonMore = document.querySelector('.big-firstrow');
-const moreInfo = document.querySelector('.test');
+const moreInfo = document.querySelector('.mc-cremonese');
+const closeButton = document.querySelector('.button-btn');
 
 buttonMore.addEventListener('click', function() {
     moreInfo.classList.toggle('test-visible');
 })
-moreInfo.addEventListener('click', function() {
+closeButton.addEventListener('click', function() {
     moreInfo.classList.toggle('test-visible');
 })
+
+var owl = $('.gallery');
+	owl.owlCarousel({
+    items:1,
+    loop:true,
+    center:true,
+    dots: false,
+    autoWidth: false,
+    animateOut: 'fadeOutLeft',
+    animateIn: 'fadeinLeft',
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        750:{
+            items:2,
+            nav:false
+        }
+    }
+});
