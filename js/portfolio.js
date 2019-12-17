8349838a2c65967a7ff628c550eea054d94bb6ed
maarekua <markiyan89@gmail.com>
2019-12-17 12:28:40 +0000

    dimkino

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,35 +1,90 @@
 const buttonMore = document.querySelector('.big-firstrow');
-const moreInfo = document.querySelector('.mc-cremonese');
+const moreInfo = document.querySelector('.portfolio-item-descr');
 const closeButton = document.querySelector('.button-btn');
 
-buttonMore.addEventListener('click', function() {
+buttonMore.addEventListener('click', function () {
     moreInfo.classList.toggle('test-visible');
 })
-closeButton.addEventListener('click', function() {
+closeButton.addEventListener('click', function () {
     moreInfo.classList.toggle('test-visible');
 })
 
-var owl = $('.gallery');
-	owl.owlCarousel({
-    items:1,
-    loop:true,
-    center:true,
-    dots: false,
-    autoWidth: false,
-    animateOut: 'fadeOutLeft',
-    animateIn: 'fadeinLeft',
-    autoplay:true,
-    autoplayTimeout:2500,
-    autoplayHoverPause:true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        750:{
-            items:2,
-            nav:false
+function addCarousel () {
+    const owl = $('.gallery');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        center: true,
+        dots: false,
+        autoWidth: true,
+        animateOut: 'fadeOutLeft',
+        animateIn: 'fadeinLeft',
+        autoplay: true,
+        autoplayTimeout: 2500,
+        autoplayHoverPause: true,
+        margin: 10,
+        marge: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 3,
+                nav: false
+            },
+            750: {
+                items: 5,
+                nav: false
+            }
         }
+    });
+}
+
+
+document.querySelector('.portfolio-item')
+    .addEventListener('click', moreInfoOpen);
+
+const gallery = document.querySelector('.gallery');
+function moreInfoOpen() {
+    const itemData = this.getAttribute('data');
+    const galleryData = gallery.getAttribute('data');
+    if (itemData !== galleryData) {
+        console.log('start')
+        moreInfoAdd(itemData)
+    } else {
+        console.log('stop')
+        return
     }
-});
+}
+
+function moreInfoAdd(item) {
+    gallery.setAttribute("data", item);
+    while (gallery.hasChildNodes()) {
+        gallery.removeChild(gallery.firstChild);
+    };
+    const descr = document.querySelector('.portfolio-descr-text');
+    fetch('json/portfolio-descr.json')
+        .then(response => response.json())
+        .then(data => {
+            const descrInfo = data[item];
+            descrInfo.images.forEach(imgSrc => {
+                const image = document.createElement('img')
+                image.src = imgSrc;
+                image.alt = descrInfo.name;
+                image.classList.add('descr-image');
+                gallery.append(image);
+                image.addEventListener('click', bigImage)
+            })
+            descr.innerText = descrInfo.info;
+            addCarousel();
+        });
+}
+
+
+
+function bigImage () {
+    const bigImageContainer = document.querySelector('.big-image');
+    while (bigImageContainer.hasChildNodes()) {
+        bigImageContainer.removeChild(bigImageContainer.firstChild);
+    };
+    bigImageContainer.append(this);
+    bigImageContainer.style.display = 'block';
+}
